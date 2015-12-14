9c5830d6d24054f3f9c19a7acd4df20b6104006e
diegocebanc <nba23james@hotmail.com>
2015-12-14 09:19:36 +0100

    bebida

---
+++ app/src/main/java/com/example/aimar/proyectohamburguesas/pedidobebida.java
@@ -0,0 +1,13 @@
+package com.example.aimar.proyectohamburguesas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pedidobebida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pedidobebida);
+    }
+}
