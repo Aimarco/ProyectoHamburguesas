5366c64cbacb75fdb6582812297df27a34732de1
Aimarco <aimarcorreyero@gmail.com>
2015-12-15 19:52:28 +0100

    spinners funcionando

--- app/src/main/java/com/example/aimar/proyectohamburguesas/DatosHamburguesa.java
+++ app/src/main/java/com/example/aimar/proyectohamburguesas/DatosHamburguesa.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by adminportatil on 10/12/2015.
@@ -18,8 +19,8 @@ public class DatosHamburguesa extends AppCompatActivity {
     private TextView datotlf;
     private Spinner sptamaño;
     private Spinner sptipocarne;
-    private EditText edttotal;
-    private float total;
+    private EditText edttotal,edtclasica,edtdobleq,edtclasiqueso,edtvegetal,edtespecial;
+    private float total,totaltipocarne,totaltamanio,clasica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,34 +34,46 @@ public class DatosHamburguesa extends AppCompatActivity {
         datonombre.setText(recibedatos.getString("nombre"));
         datodir.setText(recibedatos.getString("direccion"));
         datotlf.setText(recibedatos.getString("telefono"));
-
+        edttotal=(EditText) findViewById(R.id.edtTotal);
         //Declaramos el spinner y el array de valores para cada spinner
         sptamaño = (Spinner) findViewById(R.id.spintamaño);
         sptipocarne = (Spinner) findViewById(R.id.spicarne);
-        String[] tamaño = {"","Normal-5,50€", "Whoper-6,50€"};
+        String[] tamaño = {"Seleccione tamaño de la hamburguesa","Normal-5,50€", "Whoper-6,50€"};
         sptamaño.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tamaño));
-        String[] tcarne={"","Buey +2€","Pollo +1€ ","Ternera +3€"};
+        String[] tcarne={"Seleccione tipo de carne","Buey +2€","Pollo +1€ ","Ternera +3€"};
         sptipocarne.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tcarne));
 
         //programamos el comportamiento de los spinner(en nuestro caso cuando elija un tamaño y tipo de carne este se sumara al TOTAL
         edttotal=(EditText) findViewById(R.id.edtTotal);
 
+
+
+
         sptamaño.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+               totaltamanio=0;
                 switch (position) {
+                    case 0:
+                        totaltamanio += 0;
+                        // edttotal.setText("" + total);
+                        total = calculaTotal(totaltipocarne, totaltamanio);
+                        edttotal.setText("" + total);break;
                     case 1:
-                        total += 5.50;
+                        totaltamanio += 5.50;
                        // edttotal.setText("" + total);
+                        total = calculaTotal(totaltipocarne, totaltamanio);
+                        edttotal.setText("" + total);
+
                         break;
 
                     case 2:
-                        total += 6.50;
+                        totaltamanio += 6.50;
                        // edttotal.setText("" + total);
+                        total = calculaTotal(totaltipocarne, totaltamanio);
+                        edttotal.setText("" + total);
                         break;
                 }
-                edttotal.setText("" + total);
 
             }
     public void onNothingSelected(AdapterView<?> parent)
@@ -74,28 +87,72 @@ public class DatosHamburguesa extends AppCompatActivity {
         sptipocarne.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                totaltipocarne = 0;
                 switch (position) {
+                    case 0:
+                        totaltipocarne += 2;
+                        // edttotal.setText("" + total);
+                        total = calculaTotal(totaltipocarne, totaltamanio);
+                        edttotal.setText("" + total);break;
                     case 1:
-                        total += 2;
-                       // edttotal.setText("" + total);
+                        totaltipocarne += 2;
+                        // edttotal.setText("" + total);
+                        total = calculaTotal(totaltipocarne, totaltamanio);
+                        edttotal.setText("" + total);
                         break;
 
                     case 2:
-                        total += 1;
+                        totaltipocarne += 1;
                         //edttotal.setText("" + total);
+                        total = calculaTotal(totaltipocarne, totaltamanio);
+                        edttotal.setText("" + total);
                         break;
                     case 3:
-                        total+=3;
-                       // edttotal.setText("" + total);
+                        totaltipocarne += 3;
+                        // edttotal.setText("" + total);
+                        total = calculaTotal(totaltipocarne, totaltamanio);
+                        edttotal.setText("" + total);
                         break;
 
                 }
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                    //vacio
+                //vacio
             }
         });
 
+        //cambio de valor del edit text Total
+            //Vamos a tratar que el usuario cambie el valor de cada una de las hamburguesas posibles
+            //para ello usaremos la caracteristica whenever modifying
+            edtclasica=(EditText) findViewById(R.id.cantclasica);
+            edtclasiqueso=(EditText) findViewById(R.id.cantclasqueso);
+            edtdobleq=(EditText) findViewById(R.id.cantdoblequeso);
+            edtvegetal=(EditText) findViewById(R.id.cantvegetal);
+            edtespecial=(EditText) findViewById(R.id.cantespecial);
+
+
+    }
+
+
+    //funciones de proyecto
+
+    public static float calculaTotal(float ttipo,float ttamanio){
+        float total=0;
+        if(ttipo!=0 || ttamanio!=0)
+            total=ttipo+ttamanio;
+        else
+            total=0;
+        return total;
+    }
+    //funcion para cambiar valor de total cambiando la hamburguesa seleccionada
+    public static float totalHamburguesa(float total,float phamburguesa){
+
+        if(phamburguesa!=0)
+            total+=phamburguesa;
+        else
+           total=0;
+        return total;
     }
 }
