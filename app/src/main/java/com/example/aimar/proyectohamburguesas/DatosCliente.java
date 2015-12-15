aaa18d81bcb3de7ddd5f9d61391988de6cc912ad
Aimarco <aimarcorreyero@gmail.com>
2015-12-15 11:18:09 +0100

    15/12

--- app/src/main/java/com/example/aimar/proyectohamburguesas/DatosCliente.java
+++ app/src/main/java/com/example/aimar/proyectohamburguesas/DatosCliente.java
@@ -1,31 +1,31 @@
-
 package com.example.aimar.proyectohamburguesas;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class DatosCliente extends AppCompatActivity {
-private EditText nombre;
-private EditText direccion;
-private EditText telefono;
-private Intent mandadatoscli;
-private Button salir;
+    private EditText nombre;
+    private EditText direccion;
+    private EditText telefono;
+    private Intent mandadatoscli;
+    private Button salir;
     private Button seguir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_cliente);
-        nombre=(EditText) findViewById(R.id.edtnombre);
-        direccion=(EditText) findViewById(R.id.edtdireccion);
-        telefono=(EditText) findViewById(R.id.edttlf);
-        salir=(Button)findViewById(R.id.btnsalir);
-        seguir=(Button)findViewById(R.id.btnseguir);
-        mandadatoscli=new Intent(this,DatosHamburguesa.class);
+        nombre = (EditText) findViewById(R.id.edtnombre);
+        direccion = (EditText) findViewById(R.id.edtdireccion);
+        telefono = (EditText) findViewById(R.id.edttlf);
+        salir = (Button) findViewById(R.id.btnsalir);
+        seguir = (Button) findViewById(R.id.btnseguir);
+        mandadatoscli = new Intent(this, DatosHamburguesa.class);
 
         seguir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,8 +34,8 @@ private Button salir;
                     Toast.makeText(DatosCliente.this, "Rellene los datos por favor", Toast.LENGTH_SHORT).show();
                 else {
                     mandadatoscli.putExtra("nombre", nombre.getText().toString());
-                    mandadatoscli.putExtra("direccion",direccion.getText().toString());
-                    mandadatoscli.putExtra("telefono",telefono.getText().toString());
+                    mandadatoscli.putExtra("direccion", direccion.getText().toString());
+                    mandadatoscli.putExtra("telefono", telefono.getText().toString());
                     startActivity(mandadatoscli);
 
                 }
--- app/src/main/java/com/example/aimar/proyectohamburguesas/DatosHamburguesa.java
+++ app/src/main/java/com/example/aimar/proyectohamburguesas/DatosHamburguesa.java
@@ -2,30 +2,100 @@ package com.example.aimar.proyectohamburguesas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * Created by adminportatil on 10/12/2015.
  */
 public class DatosHamburguesa extends AppCompatActivity {
-private TextView datonombre;
+    private TextView datonombre;
     private TextView datodir;
     private TextView datotlf;
-
-
+    private Spinner sptamaño;
+    private Spinner sptipocarne;
+    private EditText edttotal;
+    private float total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedidohamburguesa);
-        datonombre=(TextView) findViewById(R.id.txtnombrepas);
-        datodir=(TextView)findViewById(R.id.txtdirecpas);
-        datotlf=(TextView) findViewById(R.id.txttlfpas);
-        Bundle recibedatos=getIntent().getExtras();
+        total=0;
+        datonombre = (TextView) findViewById(R.id.txtnombrepas);
+        datodir = (TextView) findViewById(R.id.txtdirecpas);
+        datotlf = (TextView) findViewById(R.id.txttlfpas);
+        Bundle recibedatos = getIntent().getExtras();
         datonombre.setText(recibedatos.getString("nombre"));
         datodir.setText(recibedatos.getString("direccion"));
         datotlf.setText(recibedatos.getString("telefono"));
+
+        //Declaramos el spinner y el array de valores para cada spinner
+        sptamaño = (Spinner) findViewById(R.id.spintamaño);
+        sptipocarne = (Spinner) findViewById(R.id.spicarne);
+        String[] tamaño = {"","Normal-5,50€", "Whoper-6,50€"};
+        sptamaño.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tamaño));
+        String[] tcarne={"","Buey +2€","Pollo +1€ ","Ternera +3€"};
+        sptipocarne.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tcarne));
+
+        //programamos el comportamiento de los spinner(en nuestro caso cuando elija un tamaño y tipo de carne este se sumara al TOTAL
+        edttotal=(EditText) findViewById(R.id.edtTotal);
+
+        sptamaño.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 1:
+                        total += 5.50;
+                       // edttotal.setText("" + total);
+                        break;
+
+                    case 2:
+                        total += 6.50;
+                       // edttotal.setText("" + total);
+                        break;
+                }
+                edttotal.setText("" + total);
+
+            }
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+        // vacio
+
+    }
+});
+
+//ahora tratamos el comportamiento del spinner del tipo de carne
+        sptipocarne.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 1:
+                        total += 2;
+                       // edttotal.setText("" + total);
+                        break;
+
+                    case 2:
+                        total += 1;
+                        //edttotal.setText("" + total);
+                        break;
+                    case 3:
+                        total+=3;
+                       // edttotal.setText("" + total);
+                        break;
+
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                    //vacio
+            }
+        });
+
     }
 }
